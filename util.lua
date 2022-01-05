0ed7f7ae98a45a6932e6338fb6c298f4e841f0ae
alex <alexandruturcanu915@gmail.com>
2022-01-05 09:58:30 +0300

    Formated and removed functions that already exist in the awesome lib.

--- util.lua
+++ util.lua
@@ -10,101 +10,84 @@ local util = {}
 -- Original credit goes to https://github.com/EvandroLG/array.lua for providing array util methods. Thanks EvandrolLG.
 -- Also they are here cause I only needed one method table_diff and awesome lua compiler could not see them when installed through lua rocks.
 function util.convertToHash(obj)
-    local output = {}
+  local output = {}
 
-    for i = 1, #obj do
-        local value = obj[i]
-        output[value] = true
-    end
+  for i = 1, #obj do
+    local value = obj[i]
+    output[value] = true
+  end
 
-    return output
+  return output
 end
 
 function util.tableDiff(obj1, obj2)
-    local output = {}
-    local hash = util.convertToHash(obj2)
+  local output = {}
+  local hash = util.convertToHash(obj2)
 
-    for i = 1, #obj1 do
-        local value = obj1[i]
+  for i = 1, #obj1 do
+    local value = obj1[i]
 
-        if not hash[value] then table.insert(output, value) end
-    end
+    if not hash[value] then table.insert(output, value) end
+  end
 
-    return output
+  return output
 end
 -- #endregion
 
 --[[
-    Retuns the index difference of two arrays.
-    Basically you can use what is returned as an index to see what is different in the two arrays.
-    Note this does not deal with keys just indexes.
+  Retuns the index difference of two arrays.
+  Basically you can use what is returned as an index to see what is different in the two arrays.
+  Note this does not deal with keys just indexes.
 
-    @param tbl1: First Table.
-    @param tbl2: Second Table.
-    @return Table: object with the indexes that were different.
+  @param tbl1: First Table.
+  @param tbl2: Second Table.
+  @return Table: object with the indexes that were different.
 ]]
 function util.tableDiffIndex(tbl1, tbl2)
-    local indexes = {}
-    local r = {}
-    for i, v in ipairs(tbl1) do r[i] = v == tbl2[i] end
-    for i, v in ipairs(r) do if not v then table.insert(indexes, i) end end
+  local indexes = {}
+  local r = {}
+  for i, v in ipairs(tbl1) do r[i] = v == tbl2[i] end
+  for i, v in ipairs(r) do if not v then table.insert(indexes, i) end end
 
-    return indexes
+  return indexes
 end
 
 --[[
-    Simple clamp function.
+  Simple clamp function.
 
-    @param v: Value to be clamped
-    @param min: Min value to be set to if less then.
-    @param max: Max value to be set to if more then.
-    @return: Clamp value.
+  @param v: Value to be clamped
+  @param min: Min value to be set to if less then.
+  @param max: Max value to be set to if more then.
+  @return: Clamp value.
 ]]
 function util.clamp(v, min, max)
-    return math.max(math.min(v, max), min)
+  return math.max(math.min(v, max), min)
 end
 
 --[[
-    Generates a uui. Note sure if this is specification approved however.
+  Generates a uui. Note sure if this is specification approved however.
 
-    @return: UUID string.
+  @return: UUID string.
 ]]
 function util.uuid()
-    local template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
-    return string.gsub(template, "[xy]", function(c)
-        local v = (c == "x") and random(0, 0xf) or random(8, 0xb)
-        return string.format("%x", v)
-    end)
+  return string.gsub("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx", "[xy]", function(c)
+    return string.format("%x", (c == "x") and random(0, 0xf) or random(8, 0xb))
+  end)
 end
 
 --[[
-    Makes a shallow copy of the table. I.E. References in children are kept instead of new obj being created.
+  Makes a shallow copy of the table. I.E. References in children are kept instead of new obj being created.
 
-    @param tbl: Table to be copied.
-    @return: Copied table.
+  @param tbl: Table to be copied.
+  @return: Copied table.
 ]]
 function util.shallowCopy(tbl)
-    local result = {}
-    for k, v in pairs(tbl) do result[k] = v end
+  local result = {}
+  for k, v in pairs(tbl) do result[k] = v end
 
-    for i, v in ipairs(tbl) do result[i] = v end
+  for i, v in ipairs(tbl) do result[i] = v end
 
-    return result
-end
-
---[[
-    Merges the value of two tables.
-
-    @param tbl1: The table to be merged.
-    @param tbl2: The table from which the values being merged come from.
-    @return: the merged table.
-]]
-function util.mergeTables(tbl1, tbl2)
-    for k, v in pairs(tbl2) do tbl1[k] = v end
-
-    for i, v in ipairs(tbl2) do tbl1[i] = v end
-
-    return tbl1
+  return result
 end
 
 return util
