2b453b5eef620cee7f9ca25b871bf1c4c36aa556
alex <alexandruturcanu915@gmail.com>
2022-01-05 09:58:10 +0300

    Renamed To Node, moved any generic table combining to the awesome lib methods.

--- node.lua
+++ node.lua
@@ -5,20 +5,21 @@
 -- All a tree needs to do is just have a root attribute to be considered a tree of this.
 --
 ------------------------------------------------------------------------------------------
-local util = require(TREES.relative_path .. "util")
+local util = require(TREES.relative_path .. ".util")
+local gtable = require("gears.table")
 
 local node = {}
 -- Not 100% sure why this works but it prevents certain null pointer exceptions from occuring.
 node.__index = node
 
 --[[
-    Creates a new node in the tree.
+  Creates a new node in the tree.
 
-    @param data: The client the node is holding.
-    @param left: Left node.
-    @param right: Right node.
-    @param parent: Parent node.
-    @return: Node
+  @param data: The client the node is holding.
+  @param left: Left node.
+  @param right: Right node.
+  @param parent: Parent node.
+  @return: Node
 ]]
 function node.new(data, left, right, parent)
   local newNode = {data = data, left = left, right = right, parent = parent, split = 0.5, is_vertical = false, id = util.uuid()}
@@ -26,10 +27,10 @@ function node.new(data, left, right, parent)
 end
 
 --[[
-    Adds left child node.
+  Adds left child node.
 
-    @param child: The child node to be added.
-    @return: The refereced set child node.
+  @param child: The child node to be added.
+  @return: The refereced set child node.
 ]]
 function node:addLeft(child)
   if self ~= nil then
@@ -40,10 +41,10 @@ function node:addLeft(child)
 end
 
 --[[
-    Adds right child node.
+  Adds right child node.
 
-    @param child: The child node to be added.
-    @return: The refereced set child node.
+  @param child: The child node to be added.
+  @return: The refereced set child node.
 ]]
 function node:addRight(child)
   if self ~= nil then
@@ -54,15 +55,15 @@ function node:addRight(child)
 end
 
 --[[
-    Removes the child leaf if the clients matches and sets the other node as it's self effectivly distroying itself in the process.
-    If it cannot do that then the children nodes attempt this.
+  Removes the child leaf if the clients matches and sets the other node as it's self effectivly distroying itself in the process.
+  If it cannot do that then the children nodes attempt this.
 
-    @param client: The client to match the data for.
+  @param client: The client to match the data for.
 ]]
 function node:removeLeaf(client)
   if self.left then
     if self.left.data == client then
-      local newSelf = util.shallowCopy(self.right)
+      local newSelf = gtable.copy(self.right, false)
       self.data = newSelf.data
       self.left = newSelf.left
       self.right = newSelf.right
@@ -73,7 +74,7 @@ function node:removeLeaf(client)
   end
   if self.right then
     if self.right.data == client then
-      local newSelf = util.shallowCopy(self.left)
+      local newSelf = gtable.copy(self.left, false)
       self.data = newSelf.data
       self.left = newSelf.left
       self.right = newSelf.right
@@ -86,11 +87,11 @@ function node:removeLeaf(client)
 end
 
 --[[
-    Recursivly finds the node where the client matches.
-    Nil is retunred if none can be found.
+  Recursivly finds the node where the client matches.
+  Nil is retunred if none can be found.
 
-    @param client: The client to be looked for.
-    @return: The node the client is on or nil if it failed to find any.
+  @param client: The client to be looked for.
+  @return: The node the client is on or nil if it failed to find any.
 ]]
 function node:find(client)
   if self.data then
@@ -104,21 +105,21 @@ function node:find(client)
 end
 
 --[[
-    Debug tool used to print the nodes information. With indent padding to help readability.
+  Debug tool used to print the nodes information. With indent padding to help readability.
 
-    @param node: The base node to start printing from.
-    @param level: The current level. I.E. indent offset. Default 0
-    @param prefix: String prefix before the word Node is printed. Will only display for root node as it gets overwritten for the left and right.
+  @param node: The base node to start printing from.
+  @param level: The current level. I.E. indent offset. Default 0
+  @param prefix: String prefix before the word Node is printed. Will only display for root node as it gets overwritten for the left and right.
 ]]
-function node.print(node, level, prefix)
+function node.print(n, level, prefix)
   level = level or 0
   prefix = prefix or ""
 
-  if type(node) == "table" then
+  if type(n) == "table" then
     print(string.rep(" ", level) .. prefix .. "Node:")
-    for k, v in pairs(node) do if k ~= "left" and k ~= "right" then print(string.rep(" ", level + 1) .. tostring(k) .. ": " .. tostring(v)) end end
-    if node.left ~= nil and type(node.left) == "table" then node.print(node.left, level + 1, "L_") end
-    if node.right ~= nil and type(node.right) == "table" then node.print(node.right, level + 1, "R_") end
+    for k, v in pairs(n) do if k ~= "left" and k ~= "right" then print(string.rep(" ", level + 1) .. tostring(k) .. ": " .. tostring(v)) end end
+    if n.left ~= nil and type(n.left) == "table" then n.print(n.left, level + 1, "L_") end
+    if n.right ~= nil and type(n.right) == "table" then n.print(n.right, level + 1, "R_") end
   end
 end
 
